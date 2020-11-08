37f94e9bc0a152b406f9072659d7cd113a13f01d
Valera Kineshov <Valera.Kineshov@mail.ru>
2020-11-08 22:44:21 +0300

    Урок #1

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,8 +3,26 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat myrka = new Cat();
+        Cat neko = new Cat();
+        Cat koshka = new Cat();
+        Cat godzilla = new Cat();
+        Cat kazuma = new Cat();
 
-        System.out.println(cat.getStatus());
+        System.out.println("Вес до кормёжки : " + myrka.getWeight());
+        myrka.feed(150.0);
+        System.out.println("Вес после кормёжки : " + myrka.getWeight());
+
+        System.out.println("Вес до кормёжки : " + neko.getWeight());
+        neko.drink(100.0);
+        System.out.println("Вес после кормёжки : " + neko.getWeight());
+
+        while (!koshka.getStatus().equals("Dead"))
+            koshka.meow();
+        System.out.println(koshka.getStatus());
+
+        while (!godzilla.getStatus().equals("Exploded"))
+            godzilla.feed(150.0);
+        System.out.println(godzilla.getStatus());
     }
 }
