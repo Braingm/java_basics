19ed1207cdf9d4e759c7d1f3638dc4376c54696e
Valera Kineshov <Valera.Kineshov@mail.ru>
2020-12-03 20:05:38 +0300

    V1.1

--- 04_NumbersStringsAndDates/homework_4.2/src/Main.java
+++ 04_NumbersStringsAndDates/homework_4.2/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.printf((formatSting)+  "%n","Integer",Integer.MIN_VALUE,Integer.MAX_VALUE);
         System.out.printf((formatSting)+  "%n","Long",Long.MIN_VALUE,Long.MAX_VALUE);
         formatSting = "Значения %s \nМинимальное %f\nМаксимальное %f\n";
-        System.out.printf((formatSting)+  "%n","Float",Float.MIN_VALUE,Float.MAX_VALUE);
-        System.out.printf((formatSting)+  "%n","Double",Double.MIN_VALUE,Double.MAX_VALUE);
+        System.out.printf((formatSting)+  "%n","Float",-Float.MAX_VALUE,Float.MAX_VALUE);
+        System.out.printf((formatSting)+  "%n","Double",-Double.MAX_VALUE,Double.MAX_VALUE);
     }
 }
