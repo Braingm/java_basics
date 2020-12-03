16a23caebee499505ab9885c0bf0d1ee44c1ce4a
Valera Kineshov <Valera.Kineshov@mail.ru>
2020-12-03 20:05:38 +0300

    V1.1

--- 04_NumbersStringsAndDates/homework_4.2/src/Main.java
+++ 04_NumbersStringsAndDates/homework_4.2/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         String formatSting = "Значения %s \nМинимальное %d\nМаксимальное %d\n";
-        System.out.printf((formatSting) + "%n","Byte",Byte.MIN_VALUE,Byte.MAX_VALUE);
+        System.out.printf((formatSting)+ "%n","Byte",Byte.MIN_VALUE,Byte.MAX_VALUE);
         System.out.printf((formatSting)+  "%n","Short",Short.MIN_VALUE,Short.MAX_VALUE);
         System.out.printf((formatSting)+  "%n","Integer",Integer.MIN_VALUE,Integer.MAX_VALUE);
         System.out.printf((formatSting)+  "%n","Long",Long.MIN_VALUE,Long.MAX_VALUE);
         formatSting = "Значения %s \nМинимальное %f\nМаксимальное %f\n";
-        System.out.printf((formatSting)+  "%n","Float",Float.MIN_VALUE,Float.MAX_VALUE);
-        System.out.printf((formatSting)+  "%n","Double",Double.MIN_VALUE,Double.MAX_VALUE);
+        System.out.printf((formatSting)+  "%n","Float",-Float.MAX_VALUE,Float.MAX_VALUE);
+        System.out.printf((formatSting)+  "%n","Double",-Double.MAX_VALUE,Double.MAX_VALUE);
     }
 }
