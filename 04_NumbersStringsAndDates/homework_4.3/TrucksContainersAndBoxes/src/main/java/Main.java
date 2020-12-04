e3e771d9b053e5b8b339d69cd9483a5e40367028
Valera Kineshov <Valera.Kineshov@mail.ru>
2020-12-04 14:10:27 +0300

    v 2.0 Update perfomace, now in 13ms faster !

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -6,30 +6,28 @@ public class Main {
         String boxes = scanner.nextLine();
 
         int boxesCount = Integer.parseInt(boxes);
-        int boxNumber = 1;
-        int trucks = 1;
-        int containers = 1;
+        int boxNumber = 0;
+        int trucks = 0;
+        int containers = 0;
 
         int boxesInContainer = 27;
         int containersInTruck = 12;
 
-        for (; boxNumber <= boxesCount; trucks++) {
-            System.out.print("Грузовик: " + trucks + System.lineSeparator());
-            for (int i = 0; i < containersInTruck; i++) {
-                if (boxNumber > boxesCount)
-                    break;
-                System.out.print("\tКонтейнер: " + containers + System.lineSeparator());
-                for (int j = 0; j < boxesInContainer; j++) {
-                    if (boxNumber > boxesCount)
-                        break;
-                    System.out.print("\t\tЯщик: " + boxNumber + System.lineSeparator());
-                    boxNumber++;
-                }
-                containers++;
+        while (boxNumber < boxesCount) {
+            if (boxNumber % (boxesInContainer * containersInTruck) == 0){
+                trucks++;
+                System.out.print("Грузовик: " + trucks + System.lineSeparator());
             }
+            if (boxNumber % boxesInContainer == 0){
+                containers++;
+                System.out.print("\tКонтейнер: " + containers + System.lineSeparator());
+            }
+            boxNumber++;
+            System.out.print("\t\tЯщик: " + boxNumber + System.lineSeparator());
         }
+
         System.out.println("Необходимо:");
-        System.out.println("грузовиков - " + (trucks-1) + " шт.");
-        System.out.println("контейнеров - " + (containers-1) + " шт.");
+        System.out.println("грузовиков - " + (trucks) + " шт.");
+        System.out.println("контейнеров - " + (containers) + " шт.");
     }
 }
