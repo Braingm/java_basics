f9895e9aac160b77ee77cbb869e4f00c04397a79
Valera Kineshov <Valera.Kineshov@mail.ru>
2020-12-10 17:47:52 +0300

    PhoneCleaner completed

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -2,16 +2,35 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            StringBuilder result = new StringBuilder();
+            result.append(input.replaceAll("[^0-9]", ""));
+
+            if (result.length() < 10 || result.length() > 11) {
+                System.out.println("Неверный формат номера");
+                break;
+            }
+
+            if (result.length() == 10)
+                result.insert(0, 7);
+
+            if (result.substring(0, 1).equals("8"))
+                result.replace(0, 1, "7");
+
+            if (!result.substring(0, 1).equals("7")) {
+                System.out.println("Неверный формат номера");
+                break;
+            }
+
+            System.out.println(result);
+        }
     }
-  }
 
 }
