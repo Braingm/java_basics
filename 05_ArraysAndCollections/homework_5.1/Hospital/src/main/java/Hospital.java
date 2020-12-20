9f28b352126faebef0529f91f5807b54322a4cf4
Valera Kineshov <Valera.Kineshov@mail.ru>
2020-12-20 13:45:43 +0300

    V 1.1

--- 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
+++ 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
@@ -3,6 +3,7 @@ import java.text.DecimalFormat;
 public class Hospital {
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
+
         float[] result = new float[patientsCount];
         for (int i = 0; i < patientsCount; i++) {
             result[i] = (float) ((Math.random() * (40.0 - 32.0)) + 32.0);
@@ -11,6 +12,7 @@ public class Hospital {
     }
 
     public static String getReport(float[] temperatureData) {
+
         StringBuilder report = new StringBuilder();
         float temperatureSum = 0;
         int healthyPatients = 0;
--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -1,25 +1,21 @@
+
 public class TwoDimensionalArray {
+
     public static char symbol = 'X';
 
     public static char[][] getTwoDimensionalArray(int size) {
 
         char[][] result = new char[size][size];
 
-        for (int i = 0; i < size ; i++){
-            for (int j = 0; j < size; j++){
-                if (j == i || j == (size-1) - i)
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (j == i || j == (size - 1) - i)
                     result[i][j] = symbol;
                 else {
                     result[i][j] = ' ';
                 }
             }
         }
-
-        //TODO: Написать метод, который создаст двумерный массив char заданного размера.
-        // массив должен содержать символ symbol по диагоналям, пример для size = 3
-        // [X,  , X]
-        // [ , X,  ]
-        // [X,  , X]
 
         return result;
     }
