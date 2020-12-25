5754331d52dcef5616da436d285c0ec1da6a0b0d
Valera Kineshov <Valera.Kineshov@mail.ru>
2020-12-25 14:24:51 +0300

    V 1.3

--- 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
+++ 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
@@ -2,16 +2,16 @@ import java.text.DecimalFormat;
 
 public class Hospital {
 
-    private static final int minTemperature = 32;
-    private static final int maxTemperature = 40;
-    private static final float minNormalTemperature = 36.1f;
-    private static final float maxNormalTemperature = 37f;
+    private static final int MIN_TEMPERATURE = 32;
+    private static final int MAX_TEMPERATURE = 40;
+    private static final float MIN_NORMAL_TEMPERATURE = 36.1f;
+    private static final float MAX_NORMAL_TEMPERATURE = 37f;
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
         float[] result = new float[patientsCount];
         for (int i = 0; i < patientsCount; i++) {
-            result[i] = (float) ((Math.random() * (maxTemperature - minTemperature)) + minTemperature);
+            result[i] = (float) ((Math.random() * (MAX_TEMPERATURE - MIN_TEMPERATURE)) + MIN_TEMPERATURE);
         }
         return result;
     }
@@ -25,7 +25,7 @@ public class Hospital {
         report.append("Температуры пациентов: ");
         for (float fl : temperatureData) {
             report.append(fl + " ");
-            if (fl > minNormalTemperature && fl < maxNormalTemperature ) {
+            if (fl > MIN_NORMAL_TEMPERATURE && fl < MAX_NORMAL_TEMPERATURE) {
                 healthyPatients++;
             }
             temperatureSum += fl;
