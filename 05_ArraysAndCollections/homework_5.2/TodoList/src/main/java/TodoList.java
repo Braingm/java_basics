7b3872fc41a1a3b520b77b291642a467f70e1a95
Valera Kineshov <Valera.Kineshov@mail.ru>
2020-12-26 12:55:58 +0300

    v1.0

--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
@@ -1,29 +1,56 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class TodoList {
 
+    private List<String> list = new ArrayList<>();
+
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+        list.add(list.size(), todo);
+        System.out.printf("Добавлено дело \"%s\"", todo);
+        System.out.println("");
     }
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+        if (index > list.size() + 1) {
+            System.out.println("Неверный индекс");
+            System.out.println("");
+            return;
+        }
+        if (index == list.size() + 1)
+            list.add(index - 1, todo);
+        else
+            list.add(index, todo);
+        System.out.printf("Добавлено дело \"%s\" на позицию %d", todo, index);
+        System.out.println("");
     }
 
     public void edit(String todo, int index) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+        if (index > list.size() - 1) {
+            System.out.println("Изменение не возможно, неверный индекс");
+            return;
+        }
+        System.out.printf("Дело \"%s\" заменено на \"%s\"", list.get(index), todo);
+        System.out.println("");
+        list.set(index, todo);
     }
 
     public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+        if (index > list.size() - 1) {
+            System.out.println("Неверный индекс");
+            return;
+        }
+        System.out.printf("Дело \"%s\" удалено", list.get(index));
+        System.out.println("");
+        list.remove(index);
     }
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.printf("%d - %s", i, list.get(i));
+//
+//        }
+        return (ArrayList<String>) list;
     }
 
 }
