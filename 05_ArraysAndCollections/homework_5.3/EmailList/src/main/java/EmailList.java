86b95c8966d6807fae48a8c046e0a74e71ad40f5
Valera Kineshov <Valera.Kineshov@mail.ru>
2021-01-10 14:30:37 +0300

    v 1.2 Updated type of sort

--- 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/EmailList.java
+++ 05_ArraysAndCollections/homework_5.3/EmailList/src/main/java/EmailList.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class EmailList {
-    private Set<String> emailList = new TreeSet<>();
+    private Set<String> emailList = new TreeSet<>(new EmailComparator());
 
     public void add(String email) {
         if (email.matches(".+@.+\\.+.+"))
@@ -10,5 +10,16 @@ public class EmailList {
 
     public List<String> getSortedEmails() {
         return new ArrayList<>(emailList);
+    }
+}
+
+class EmailComparator implements Comparator <String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        int result = o1.substring(o1.indexOf("@")).compareTo(o2.substring(o2.indexOf("@")));
+        if (result == 0)
+            return o1.compareTo(o2);
+        return result;
     }
 }
