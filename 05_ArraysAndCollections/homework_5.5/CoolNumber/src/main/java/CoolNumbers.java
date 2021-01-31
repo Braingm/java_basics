3fa032dfc5875f0a6fa2b4d8cef50adb24c23fc6
Valera Kineshov <Valera.Kineshov@mail.ru>
2021-01-31 12:33:58 +0300

    V1.1

--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,50 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
+    public static List<String> coolNumbers;
+
+    static {
+        coolNumbers = generateCoolNumbers();
+    }
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        List<String> numbers = new ArrayList<>();
+        char[] letters = {'А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'У', 'Х'};
+
+        for (char s1 : letters) {
+            for (char s2 : letters) {
+                for (char s3 : letters) {
+                    for (int i = 0; i < 10; i++) {
+                        for (int r = 1; r <= 199; r++) {
+                            String region = String.valueOf(r);
+                            if (r <= 9) {
+                                region = "0" + region;
+                            }
+                            String number = String.format("%s%d%d%d%s%s%s", s1, i, i, i, s2, s3, region);
+                            numbers.add(number);
+                        }
+                    }
+                }
+            }
+        }
+        return numbers;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        return list.contains(number);
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        return Collections.binarySearch(sortedList, number) >= 0;
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        return hashSet.contains(number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        return treeSet.contains(number);
     }
 
 }
--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
@@ -1,12 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.*;
+
 public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
+    private static final String SUCCESS = "Номер найден, поиск занял (нс) : ";
+    private static final String FAILURE = "Номер не найден, поиск занял (нс) : ";
+    static long nanoTime;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<String> hashSet = new HashSet<>(CoolNumbers.coolNumbers);
+        TreeSet<String> treeSet = new TreeSet<>(CoolNumbers.coolNumbers);
+        ArrayList<String> arrayList = new ArrayList<>(CoolNumbers.coolNumbers);
+        ArrayList<String> bruteArrayList = new ArrayList<>(CoolNumbers.coolNumbers);
+        Collections.sort(arrayList);
 
+        while (true){
+            String input = reader.readLine();
+
+            if (input.equals("0")){
+                break;
+            }
+
+            nanoTime = System.nanoTime();
+            if (CoolNumbers.bruteForceSearchInList(bruteArrayList,input)){
+                System.out.println("Поиск перебором: " + SUCCESS + getNanos());
+            } else {
+                System.out.println("Поиск перебором: " + FAILURE + getNanos());
+            }
+
+            nanoTime = System.nanoTime();
+            if (CoolNumbers.binarySearchInList(arrayList,input)){
+                System.out.println("Бинарный поиск: " + SUCCESS + getNanos());
+            } else {
+                System.out.println("Бинарный поиск: " + FAILURE + getNanos());
+            }
+
+            nanoTime = System.nanoTime();
+            if (CoolNumbers.searchInHashSet(hashSet,input)){
+                System.out.println("Поиск в HashSet : " + SUCCESS + getNanos());
+            } else {
+                System.out.println("Поиск в HashSet : " + FAILURE + getNanos());
+            }
+
+            nanoTime = System.nanoTime();
+            if (CoolNumbers.searchInTreeSet(treeSet,input)){
+                System.out.println("Поиск в TreeSet : " + SUCCESS + getNanos());
+            } else {
+                System.out.println("Поиск в TreeSet : " + FAILURE + getNanos());
+            }
+
+        }
+    }
+
+
+    private static long getNanos(){
+        return System.nanoTime() - nanoTime;
     }
 }
