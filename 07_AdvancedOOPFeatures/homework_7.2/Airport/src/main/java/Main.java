ddb1aff46dff4a849a25c9eaba3b6c16c5c82a21
Valera Kineshov <Valera.Kineshov@mail.ru>
2021-04-03 13:34:01 +0300

    V 1.0

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+
+import static com.skillbox.airport.Flight.Type.DEPARTURE;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,8 +14,18 @@ public class Main {
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        List<Flight> result = new ArrayList<>();
+
+        Calendar datePlusTwoHour = Calendar.getInstance();
+        datePlusTwoHour.add(Calendar.HOUR, 2);
+
+        airport.getTerminals()
+                .forEach(terminal -> terminal.getFlights().stream()
+                        .filter(flight -> flight.getType() == DEPARTURE)
+                        .filter(flight -> flight.getDate().before(datePlusTwoHour.getTime()))
+                        .forEach(result::add));
+
+        return result;
     }
 
 }
