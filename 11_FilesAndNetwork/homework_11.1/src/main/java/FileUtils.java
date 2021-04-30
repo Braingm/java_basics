ef7e2db5b6912c270fe1a7ed6745039ec5403d36
Valera Kineshov <Valera.Kineshov@mail.ru>
2021-04-30 17:36:59 +0300

    V 1.0

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -1,6 +1,31 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        Path directoryPath = Paths.get(path);
+        long size = 0;
+        try (Stream<Path> walk = Files.walk(directoryPath)) {
+            size = walk
+                    .filter(Files::isRegularFile)
+                    .mapToLong(p -> {
+                        try {
+                            return Files.size(p);
+                        } catch (IOException e) {
+                            Main.logger.error(e);
+                            System.out.printf("Невозможно получить размер файла %s%n%s", p, e);
+                            return 0L;
+                        }
+                    })
+                    .sum();
+        } catch (IOException e) {
+            Main.logger.error(e);
+            System.out.printf("Ошибка при подсчёте размера директории %s", e);
+        }
+        return size;
     }
 }
--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,5 +1,63 @@
-public class Main {
-    public static void main(String[] args) {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    //Инициализация листа
+    private static final List<String> memoryUnits = new ArrayList();
+    public static Logger logger = LogManager.getRootLogger();
+
+    static {
+        memoryUnits.add("Б");
+        memoryUnits.add("Кб");
+        memoryUnits.add("Гб");
+        memoryUnits.add("Тб");
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            var reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Введите путь к папке:");
+            String path = "";
+            try {
+                path = reader.readLine();
+            } catch (IOException e) {
+                logger.error(e);
+                System.out.println("Ошибка ввода");
+                continue;  //При возникновении ошибки начинает цикл сначала. Гарантирует инициализацию path
+            }
+            File directory = new File(path);
+            if (!directory.isDirectory()) {
+                System.out.println("Введён путь к файлу");
+            }
+            if (!directory.exists()) {
+                System.out.println("Папка не существует");
+            }
+
+            double folderSize = (double) FileUtils.calculateFolderSize(path);
+            int unitOrder = 0;
+            while (folderSize > 1024.0) {
+                folderSize /= 1024.0;
+                unitOrder++;
+            }
+            if (folderSize >= 1000.0) {
+                unitOrder++;
+                folderSize /= 1000.0;
+            }
+
+            try {
+                System.out.printf("Размер папки %s составляет %s %s%n", path, new DecimalFormat("0.0").format(folderSize), memoryUnits.get(unitOrder));
+            } catch (Exception e) {
+                System.out.println("Произошла ошибка");
+                logger.error(e);
+            }
+        }
     }
 }
