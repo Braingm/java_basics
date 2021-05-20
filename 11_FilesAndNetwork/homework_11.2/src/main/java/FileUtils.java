96e1a0d03fab87c68b43454b5eeef4959b54f796
Valera Kineshov <Valera.Kineshov@mail.ru>
2021-05-20 20:12:03 +0300

    20.05 v1

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+        Path source = Paths.get(sourceDirectory);
+        Path destination = Paths.get(destinationDirectory);
+
+        Files.walkFileTree(source, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path directory, BasicFileAttributes attrs) throws IOException {
+                Path destinationSubDir = destination.resolve(source.relativize(directory));
+                Files.createDirectories(destinationSubDir);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Path destinationFile = destination.resolve(source.relativize(file));
+                Files.copy(file, destinationFile);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
     }
 }
--- 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String source = null;
+        String destination = null;
+
+        try {
+            System.out.println("Введите папку для копирования");
+            source = reader.readLine();
+            System.out.println("Введите папку назначения");
+            destination = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileUtils.copyFolder(source,destination);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
