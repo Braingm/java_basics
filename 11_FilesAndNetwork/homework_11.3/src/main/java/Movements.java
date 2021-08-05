556433608e4efc3d0582fa9c1433c90e8df715a6
Valera Kineshov <Valera.Kineshov@mail.ru>
2021-08-05 22:24:46 +0300

    V 1.0

--- 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
+++ 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
@@ -1,10 +1,19 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Movements {
+    public static List<Movement> movements = new ArrayList<>();
 
     public Movements(String pathMovementsCsv) {
     }
 
     public double getExpenseSum() {
-        return 0.0;
+        Set<Double> sumSet = new HashSet();
+        movements.forEach(movement -> sumSet.add(movement.getOutcome().doubleValue()));
+        return sumSet.stream().reduce(Double::sum).get();
     }
 
     public double getIncomeSum() {
