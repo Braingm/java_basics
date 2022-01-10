c13a8eb0343f0e4af1333458783089d4eb658a10
Valera Kineshov <Valera.Kineshov@mail.ru>
2022-01-10 17:43:38 +0300

    Homework 14.1 v2

--- 14_SQLAndHibernate/MySql/src/main/java/Main.java
+++ 14_SQLAndHibernate/MySql/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -8,33 +9,37 @@ public class Main {
         String user = "root";
         String pass = "root";
 
-        try {
-            Connection connection = DriverManager.getConnection(url, user, pass);
+        int year = 2018;
 
-            Statement statement = connection.createStatement();
-            var result = statement.executeQuery("""
-                    select courses.name,\s
+        try (Connection connection = DriverManager.getConnection(url, user, pass)) {
 
-                    (select count(*) from purchaselist where purchaselist.course_name = courses.name)
-                    /
-                    (
-                    (select month(purchaselist.subscription_date)  from purchaselist where purchaselist.course_name = courses.name order by subscription_date desc limit 1) -\s
-                    (select month(purchaselist.subscription_date) from purchaselist where purchaselist.course_name = courses.name order by subscription_date limit 1) + 1
-                    )  as sell_results
+            String sql = """                 
+                       select courses.name,
+                                       
+                       (select count(*) from purchaselist where purchaselist.course_name = courses.name)
+                       /
+                       (
+                       (select month(purchaselist.subscription_date)  from purchaselist where purchaselist.course_name = courses.name order by subscription_date desc limit 1) -\s
+                       (select month(purchaselist.subscription_date) from purchaselist where purchaselist.course_name = courses.name order by subscription_date limit 1) + 1
+                       ) as sell_results
+                                       
+                       from courses
+                       join purchaselist on purchaselist.course_name = courses.name
+                       where year(purchaselist.subscription_date) = ?
+                       group by courses.name
+                    """;
 
-                    from courses
-                    join purchaselist on purchaselist.course_name = courses.name
-                    group by courses.name"""
-            );
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, year);
+
+            var result = preparedStatement.executeQuery();
+
             while (result.next()) {
                 String courseName = result.getString("name");
                 var sellResults = result.getFloat("sell_Results");
                 System.out.println(courseName + " - " + sellResults);
             }
 
-            connection.close();
-            statement.close();
-            result.close();
         } catch (Exception ignored) {
         }
     }
